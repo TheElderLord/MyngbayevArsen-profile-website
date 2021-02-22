0e898b25197819db65fff97e2a613785341b7c1f
Elder Lord <190103402@stu.sdu.edu.kz>
2021-02-22 13:11:05 +0600

    routes

--- MyWeb/routes/web.php
+++ MyWeb/routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/main',function(){
+return view('main');
+})->name('main');
+
+Route::get('/about',function() {
+return view('about');
+})->name('about');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
