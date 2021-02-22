683698c9c9a0c52960d8cdfda045a0a376901387
Elder Lord <190103402@stu.sdu.edu.kz>
2021-02-22 16:15:44 +0600

    Main page

---
+++ MyWeb/resources/views/main.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Akaya+Telivigala&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/main.css">
+</head>
+<body>
+    <div class="header" >
+        <div class="links">
+    <a href="{{route('main')}}">Main</a>
+    <a href="{{route('about')}}">About Us</a>
+    <a href="{{route('contact')}}">Contacts</a></div>
+    <div class="name"><h1>Welcome to Webpage</h1></div></div>
+  
+    <div class="mainBody">
+       
+        <div class="mainimg">
+            <img src="/images/smaa.png" alt="" width="500px">
+
+        </div>
+        <div class="maintext">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo architecto totam eveniet distinctio soluta error ratione maiores molestiae magni. Qui ipsa nostrum at quibusdam voluptates maiores delectus nulla molestiae eveniet.
+          </div>
+        
+    </div>
+</body>
+</html>
